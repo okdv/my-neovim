e47d595087249b5890812199005676a69720bd88
otho <65867719+okdv@users.noreply.github.com>
2022-10-13 11:16:33 -0500

    Update plugins.lua

--- lua/okdv/plugins.lua
+++ lua/okdv/plugins.lua
@@ -36,32 +36,14 @@ return packer.startup(function(use)
 
 	use({ "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" }) -- Have packer manage itself
 	use({ "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" }) -- Useful lua functions used by lots of plugins
-	use({ "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" })
-	use({ "kyazdani42/nvim-tree.lua", commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243" })
 	use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
 	use({ "nvim-lualine/lualine.nvim", commit = "3362b28f917acc37538b1047f187ff1b5645ecdd" })
-	use({ "akinsho/toggleterm.nvim", commit = "aaeed9e02167c5e8f00f25156895a6fd95403af8" })
 	use({ "lewis6991/impatient.nvim", commit = "969f2c5c90457612c09cf2a13fee1adaa986d350" })
 	use({ "lukas-reineke/indent-blankline.nvim", commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2" })
   use({ "prettier/vim-prettier", run = "npm install" })
-  use("neovim/nvim-lspconfig")
-  use("nvim-lua/completion-nvim")
-  use("anott03/nvim-lspinstall")
 
 	-- Colorschemes
   use('Mofiqul/dracula.nvim')
-
-	-- Telescope
-	use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
-
-	-- Git
-	use({ "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" })
-
-  -- Treesitter
-  use({'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"})
-  use("sheerun/vim-polyglot")
-  use('pangloss/vim-javascript')
-  use('maxmellon/vim-jsx-pretty')
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	if packer_bootstrap then require("packer").sync() end
